59f10008f4fd665deb99faf320338ac6b18ced53
ShibelAl <shibelalshech300@gmail.com>
2024-08-04 18:37:53 +0300

    added docstrings and corrected attribute value

--- logic/api/entities/project.py
+++ logic/api/entities/project.py
@@ -1,27 +1,48 @@
 class ProjectEntity:
+    """
+    Represents a project entity with a name and a workspace.
+    """
+
     def __init__(self, name, workspace):
-        self._name = name
+        """
+        Initializes the project entity with a name and workspace.
+        """
+        self._name = None
         self._workspace = None
         self.set_name(name)
         self.set_workspace(workspace)
 
     def set_name(self, name):
+        """
+        Sets the name of the project.
+        """
         self._name = name
 
     def set_workspace(self, workspace):
+        """
+        Sets the workspace of the project.
+        """
         self._workspace = workspace
 
     def get_name(self):
+        """
+        Returns the name of the project.
+        """
         return self._name
 
     def get_workspace(self):
+        """
+        Returns the workspace of the project.
+        """
         return self._workspace
 
     def to_dict(self):
+        """
+        Returns the project entity as a dictionary.
+        """
         return {
             "data": {
                 "name": self.get_name(),
                 "workspace": self.get_workspace()
             }
         }
-
