121a161907436618b6ef5e0551480ff8df698869
ShibelAl <shibelalshech300@gmail.com>
2024-08-07 13:45:41 +0300

    modified the get and post requests to utilize the response wrapper

--- infra/api/api_wrapper.py
+++ infra/api/api_wrapper.py
@@ -1,17 +1,44 @@
 import requests
 
+from infra.api.response_wrapper import ResponseWrapper
+
 
 class APIWrapper:
     def __init__(self):
         self._request = None
 
     @staticmethod
-    def get_request(url, header, querystring=None):
-        return requests.get(url, headers=header, params=querystring)
+    def get_request(url, header=None, json=None):
+        try:
+            response = requests.get(
+                url,
+                headers=header,
+                json=json
+            )
+            response.raise_for_status()
+            return ResponseWrapper(ok=response.ok, status=response.status_code, data=response.json())
+        except requests.exceptions.HTTPError as e:
+            print(f'HTTP error occurred: {e}')
+        except Exception as e:
+            print(f'Other error occurred: {e}')
+        return None
 
     @staticmethod
-    def post_request(url, header=None, body=None):
-        return requests.post(url, headers=header, json=body)
+    def post_request(url, headers=None, json=None):
+
+        try:
+            response = requests.post(
+                url,
+                headers=headers,
+                json=json
+            )
+            response.raise_for_status()
+            return ResponseWrapper(ok=response.ok, status=response.status_code, data=response.json())
+        except requests.exceptions.HTTPError as e:
+            print(f'HTTP error occurred: {e}')
+        except Exception as e:
+            print(f'Other error occurred: {e}')
+        return None
 
     @staticmethod
     def delete_request(url, header=None):
