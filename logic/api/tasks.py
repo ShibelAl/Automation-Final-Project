00fc1d3aed422c047be9aba6c3046648b92aebd7
ShibelAl <shibelalshech300@gmail.com>
2024-08-07 22:58:25 +0300

    logic file for handling tasks-related tests

---
+++ logic/api/tasks.py
@@ -0,0 +1,63 @@
+from infra.config_provider import ConfigProvider
+from logic.api.entities.task import TaskEntity
+
+
+class Tasks:
+    """
+    A class to handle tasks-related operations, such as creating, updating, and deleting tasks.
+    """
+    REQUEST_URL_ENDPOINT = "tasks"
+    GET_TASKS_ENDPOINT = "?project="
+    MY_WORKSPACE = "1207971857090881"
+
+    def __init__(self, request):
+        """
+        Initializes with a request object.
+
+        :param request: The request object used to make API calls.
+        """
+        self._request = request
+        self._config = ConfigProvider().load_config_json()
+        self._secret = ConfigProvider().load_secret_json()
+
+    def create_a_task(self, task_name, project_gid):
+        """
+        Creates a new task in the specified project.
+
+        :param task_name: The name of the task to be created.
+        :param project_gid: The global ID of the project in which to create the task.
+        :return: The response from the API call to create the task.
+        """
+        body = TaskEntity(task_name, project_gid)
+        url = f"{self._config['base_url_api']}{self.REQUEST_URL_ENDPOINT}"
+        return self._request.post_request(url, self._secret['headers_with_content'], body.to_dict())
+
+    def get_multiple_tasks(self, project_gid):
+        """
+        Retrieves multiple tasks for the specified project.
+
+        :param project_gid: The global ID of the project for which to retrieve tasks.
+        :return: The response from the API call to get the tasks.
+        """
+        url = f"{self._config['base_url_api']}{self.REQUEST_URL_ENDPOINT}{self.GET_TASKS_ENDPOINT}{project_gid}"
+        return self._request.get_request(url, self._secret['headers_without_content'])
+
+    def delete_a_task(self, task_gid):
+        """
+        Deletes a task with the specified global ID.
+
+        :param task_gid: The global ID of the task to be deleted.
+        :return: The response from the API call to delete the task.
+        """
+        url = f"{self._config['base_url_api']}{self.REQUEST_URL_ENDPOINT}/{task_gid}"
+        return self._request.delete_request(url, self._secret['headers_without_content'])
+
+    @staticmethod
+    def tasks_names(existing_tasks):
+        """
+        Extracts the names of tasks from the given list of tasks.
+
+        :param existing_tasks: The list of existing tasks.
+        :return: A list of task names.
+        """
+        return [task['name'] for task in existing_tasks['data']]
