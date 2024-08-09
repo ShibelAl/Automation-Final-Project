3ea36254a07301c48f09c8ded1d49eeb6ae885a0
ShibelAl <shibelalshech300@gmail.com>
2024-08-09 23:54:23 +0300

    returned a function that was mistakenly deleted

--- logic/api_and_ui/base_page_app.py
+++ logic/api_and_ui/base_page_app.py
@@ -20,6 +20,20 @@ class BasePageApp(BasePage):
         """
         super().__init__(driver)
 
+    def project_is_displayed(self, project_name):
+        """
+        Checks if a project with the specified name is displayed in the project list.
+        :param project_name: The name of the project to check.
+        :return: True if the project is displayed, False otherwise.
+        """
+        projects = WebDriverWait(self._driver, 10).until(
+            EC.presence_of_all_elements_located((By.XPATH, self.PROJECT_LIST))
+        )
+        for project in projects:
+            if project_name in project.get_attribute("aria-label"):
+                return True
+        return False
+
     def project_is_not_displayed(self, project_name):
         """
         Checks if a project with the specified name is not displayed in the project list.
