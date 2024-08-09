f0573a689b184faa7764470504eb158aa96d2b45
ShibelAl <shibelalshech300@gmail.com>
2024-08-09 14:54:49 +0300

    Utility class for logic and test functions

---
+++ logic/utils.py
@@ -0,0 +1,15 @@
+import random
+
+
+class Utils:
+    """
+    Utility class for logic and test functions.
+    """
+    @staticmethod
+    def generate_random_binary():
+        """
+        Generates a random number that is either 0 or 1.
+
+        :return: 0 or 1 (randomly chosen).
+        """
+        return random.randint(0, 1)
