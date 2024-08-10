1f4ff1a20535d988ca55a30bed15b95d2bd5c5b6
ShibelAl <shibelalshech300@gmail.com>
2024-08-10 22:43:42 +0300

    changed wait time

--- logic/ui/new_project_page.py
+++ logic/ui/new_project_page.py
@@ -6,10 +6,11 @@ from infra.ui.base_page import BasePage
 
 class NewProjectPage(BasePage):
     BLANK_PROJECT_BUTTON = '//div[@class = "DashedTile DashedTile--large FlowPickerTile-dashedTile"]'
+    WAIT_TIME = 20
 
     def __init__(self, driver):
         super().__init__(driver)
-        self._blank_project_button = WebDriverWait(self._driver, 5).until(
+        self._blank_project_button = WebDriverWait(self._driver, self.WAIT_TIME).until(
             EC.presence_of_element_located((By.XPATH, self.BLANK_PROJECT_BUTTON))
         )
 
@@ -18,5 +19,5 @@ class NewProjectPage(BasePage):
         Clicks on the "Blank project" button.
         """
         self._blank_project_button.click()
-        WebDriverWait(self._driver, 5).until(
+        WebDriverWait(self._driver, self.WAIT_TIME).until(
             EC.url_to_be("https://app.asana.com/0/projects/new/blank"))
