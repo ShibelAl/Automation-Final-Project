6d6fe0979e8559d84fd68230f215ed50c4ea26db
ShibelAl <shibelalshech300@gmail.com>
2024-08-11 12:35:59 +0300

    changed log file location

--- infra/logging_setup.py
+++ infra/logging_setup.py
@@ -6,7 +6,7 @@ class LoggingSetup:
     A class to manage logging configuration for the project.
     """
     logging.basicConfig(
-        filename="../log_file.log",
+        filename="../../log_file.log",
         level=logging.INFO,
         format='%(asctime)s: %(levelname)s: %(message)s',
         datefmt='%d-%m-%Y - %H:%M:%S',  # Exclude milliseconds
