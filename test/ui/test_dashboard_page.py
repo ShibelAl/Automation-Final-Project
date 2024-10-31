556b282732bd76a922fc66f09fad03bc3378c596
ShibelAl <shibelalshech300@gmail.com>
2024-10-31 23:12:43 +0200

    improved code aesthetics and utilized the infrastructure more, for more stable test file

--- test/ui/test_dashboard_page.py
+++ test/ui/test_dashboard_page.py
@@ -1,13 +1,6 @@
 import unittest
-
-from infra.jira_handler import JiraHandler
-from infra.test_failure_handler import TestFailureHandler
-from logic.ui.login_page import LoginPage
-from infra.config_provider import ConfigProvider
-from infra.ui.browser_wrapper import BrowserWrapper
-from logic.ui.sidebar import SideBar
-from logic.ui.reporting_page import ReportingPage
-from logic.ui.dashboard_page import DashboardPage
+from infra.jira_bug_reporter import JiraBugReporter
+from logic.ui.page_navigation import PageNavigation
 from logic.ui.chart_location_options import ChartLocationOptions
 
 
@@ -18,34 +11,28 @@ class TestDashboardPage(unittest.TestCase):
         Sets up the testing environment, completes the login process to enter to the main page,
         and creates a new blank project. Works automatically.
         """
-        self.browser = BrowserWrapper()
-        self.jira_handler = JiraHandler()
-        self.config = ConfigProvider.load_config_json()
-        self.secret = ConfigProvider.load_secret_json()
-        self.driver = self.browser.get_driver(self.config["base_url_app"])
-        self.login_page = LoginPage(self.driver)
-        self.login_page.login_flow(self.config["asana_email"], self.secret["asana_password"])
-        self.sidebar = SideBar(self.driver)
-        self.sidebar.click_on_reporting_button()
-        self.reporting_page = ReportingPage(self.driver)
-        self.reporting_page.go_to_dashboard_page()
+        self.page_navigation = PageNavigation()
+        self.dashboard_page = self.page_navigation.go_to_dashboard_page()
 
     def tearDown(self):
         """
         Closes the browser after completing the test.
-        Works automatically.
         """
-        self.driver.quit()
+        self.page_navigation.close_browser()
 
-    @TestFailureHandler.handle_test_failure
-    def test_drag_and_drop_feature(self):
+    @JiraBugReporter.report_bug(
+        description="The drag-and-drop functionality for recommended charts on the dashboard page is not "
+                    "operating as intended. When attempting to drag one chart and drop it onto another, "
+                    "the two charts should switch places, but they do not.",
+        priority="Medium",
+        labels=["UI", "drag_and_drop", "Charts", "Dashboard"]
+    )
+    def test_recommended_charts_drag_and_drop_feature(self):
         """
-        This function tests the drag and drop feature in the dashboard
-        page, it uses the "get_chart_location" function that returns the
-        location of the element (chart) in the page.
+        This function tests the drag and drop feature in the dashboard page for the "Recommended" charts.
+        It uses "get_chart_location()" that returns the location of the chart in the page.
         """
         # Arrange
-        self.dashboard_page = DashboardPage(self.driver)
         self.dashboard_page.add_two_charts_flow()
 
         # Capture initial state
@@ -60,5 +47,7 @@ class TestDashboardPage(unittest.TestCase):
         final_right_chart_location = self.dashboard_page.get_chart_location(ChartLocationOptions.RIGHT.value)
 
         # Assert
-        self.assertDictEqual(final_left_chart_location, initial_right_chart_location)
-        self.assertDictEqual(final_right_chart_location, initial_left_chart_location)
+        self.assertDictEqual(final_left_chart_location, initial_right_chart_location,
+                             "Left chart was not successfully moved to the right position.")
+        self.assertDictEqual(final_right_chart_location, initial_left_chart_location,
+                             "Right chart was not successfully moved to the left position.")
