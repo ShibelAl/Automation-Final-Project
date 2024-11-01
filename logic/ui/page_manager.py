27c57ab71776f396899a34947c3d0c384a1af690
ShibelAl <shibelalshech300@gmail.com>
2024-11-01 23:17:18 +0200

    renamed file, now it is page_manager, and added get functions

--- logic/ui/page_manager.py
+++ logic/ui/page_manager.py
@@ -1,18 +1,33 @@
 from infra.config_provider import ConfigProvider
 from infra.ui.browser_wrapper import BrowserWrapper
+from logic.ui.base_page_app import BasePageApp
 from logic.ui.sidebar import SideBar
 from logic.ui.reporting_page import ReportingPage
 from logic.ui.dashboard_page import DashboardPage
 
 
-class PageNavigation:
+class PageManager:
     def __init__(self):
         self._browser = BrowserWrapper()
         self._config = ConfigProvider.load_config_json()
         self._driver = self._browser.get_driver(self._config["base_url_app"])
 
+    def get_config(self, key):
+        return self._config[f"{key}"]
+
+    def get_driver(self):
+        return self._driver
+
     def close_browser(self):
         self._browser.close_browser()
+
+    def go_to_base_page_app(self):
+        """
+        Navigates to the home page - AKA base page app.
+
+        :return: BasePageApp instance representing the home page.
+        """
+        return BasePageApp(self._driver)
 
     def go_to_dashboard_page(self):
         """
