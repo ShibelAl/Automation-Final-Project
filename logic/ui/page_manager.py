ff74b95d6e4af1864f9edf2141b1316918d9a125
ShibelAl <shibelalshech300@gmail.com>
2024-11-02 11:19:52 +0200

    added a function to go to the blank project page

--- logic/ui/page_manager.py
+++ logic/ui/page_manager.py
@@ -2,6 +2,8 @@ from infra.config_provider import ConfigProvider
 from infra.ui.browser_wrapper import BrowserWrapper
 from logic.ui.base_page_app import BasePageApp
 from logic.ui.sidebar import SideBar
+from logic.ui.new_project_page import NewProjectPage
+from logic.ui.blank_project_page import BlankProjectPage
 from logic.ui.reporting_page import ReportingPage
 from logic.ui.dashboard_page import DashboardPage
 
@@ -29,16 +31,25 @@ class PageManager:
         """
         return BasePageApp(self._driver)
 
+    def go_to_blank_project_page(self):
+        """
+        Navigates to the blank project page.
+        First, go to base page app, and open a new project, and then pick the blank project option.
+
+        :return: BlankProjectPage instance representing the blank project page.
+        """
+        self.go_to_base_page_app().open_new_project()
+        NewProjectPage(self._driver).click_on_blank_project_button()
+
+        return BlankProjectPage(self._driver)
+
     def go_to_dashboard_page(self):
         """
         Navigates directly to the dashboard page.
 
         :return: DashboardPage instance representing the dashboard page.
         """
-        sidebar = SideBar(self._driver)
-        sidebar.click_on_reporting_button()
-
-        reporting_page = ReportingPage(self._driver)
-        reporting_page.go_to_dashboard_page()
+        SideBar(self._driver).click_on_reporting_button()
+        ReportingPage(self._driver).go_to_dashboard_page()
 
         return DashboardPage(self._driver)
