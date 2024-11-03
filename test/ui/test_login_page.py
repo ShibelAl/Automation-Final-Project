acee4a54ff6e976667de242c1c41f999bb630244
ShibelAl <shibelalshech300@gmail.com>
2024-11-03 16:56:04 +0200

    improved the code stability and test design + added detailed jira docstring reports

--- test/ui/test_login_page.py
+++ test/ui/test_login_page.py
@@ -1,86 +1,78 @@
 import unittest
-from selenium.common import TimeoutException
-from infra.jira_handler import JiraHandler
-from infra.test_failure_handler import TestFailureHandler
+from infra.jira_bug_reporter import JiraBugReporter
 from logic.ui.login_page import LoginPage
 from infra.config_provider import ConfigProvider
 from infra.ui.browser_wrapper import BrowserWrapper
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as ec
 
 
 class TestLoginPage(unittest.TestCase):
-    HOME_PAGE_URL = "https://app.asana.com/0/home/1207971857090891"
-    WAIT_TIME = 20
-    NEGATIVE_TEST_WAIT_TIME = 5
 
     def setUp(self):
         """
         Sets up the testing environment, opens the browser on the login page.
         Works automatically.
         """
-        self.browser = BrowserWrapper()
-        self.jira_handler = JiraHandler()
-        self.config = ConfigProvider.load_config_json()
-        self.secret = ConfigProvider.load_secret_json()
-        self.driver = self.browser.get_driver(self.config["base_url_app"])
+        self._browser = BrowserWrapper()
+        self._config = ConfigProvider.load_config_json()
+        self._secret = ConfigProvider.load_secret_json()
+        self._driver = self._browser.get_driver(self._config["base_url_app"], login=True)
+        self._login_page = LoginPage(self._driver)
 
     def tearDown(self):
         """
         Closes the browser after completing the test.
         Works automatically.
         """
-        self.driver.quit()
+        self._browser.close_browser()
 
-    @TestFailureHandler.handle_test_failure
+    @JiraBugReporter.report_bug(
+        description="The login flow is not functioning correctly. After entering a correct email and password, "
+                    "the main page of the website should be displayed, but it is not.",
+        priority="Highest",
+        labels=["Login", "Authentication", "UI"]
+    )
     def test_correct_login_flow(self):
         """
         This function tests if after inserting a correct email and password
         the main page of the website appears.
         """
-        # Arrange
-        login_page = LoginPage(self.driver)
-
         # Act
-        login_page.login_flow(self.config["asana_email"], self.secret["asana_password"])
-        WebDriverWait(self.driver, self.WAIT_TIME).until(ec.url_to_be(self.HOME_PAGE_URL))
+        self._login_page.login_flow(self._config["asana_email"], self._secret["asana_password"])
 
         # Assert
-        self.assertEqual(self.driver.current_url, self.HOME_PAGE_URL)
+        self.assertEqual(self._driver.current_url, self._config["base_url_app"])
 
-    @TestFailureHandler.handle_test_failure
-    def test_wrong_password_in_login(self):
-        """
-        This function tests if when inserting a correct email and wrong password,
-        then the login page prevents entering the website, as it should.
-        """
-        # Arrange
-        login_page = LoginPage(self.driver)
-
-        # Act
-        login_page.login_flow(self.config["asana_email"], self.config["wrong_password"])
-        try:
-            WebDriverWait(self.driver, self.NEGATIVE_TEST_WAIT_TIME).until(ec.url_to_be(self.HOME_PAGE_URL))
-        except TimeoutException:
-
-            # Assert
-            self.assertNotEqual(self.driver.current_url, self.HOME_PAGE_URL)
-
-    @TestFailureHandler.handle_test_failure
+    @JiraBugReporter.report_bug(
+        description="When a wrong email is entered, the login page shouldn't show an option to enter the password, "
+                    "this test didn't pass because after entering a wrong email, the password field is rendered.",
+        priority="Highest",
+        labels=["Login", "Authentication", "UI"]
+    )
     def test_wrong_email_in_login(self):
         """
         This function tests if when inserting a wrong email, then
         the login page prevents entering the website, as it should.
         """
-        # Arrange
-        login_page = LoginPage(self.driver)
-
         # Act
-        login_page.fill_email_input(self.config["wrong_email"])
-        login_page.click_on_continue_button()
-        try:
-            WebDriverWait(self.driver, self.NEGATIVE_TEST_WAIT_TIME).until(ec.url_to_be(self.HOME_PAGE_URL))
-        except TimeoutException:
+        self._login_page.fill_email_input(self._config["wrong_email"])
+        self._login_page.click_on_continue_button()
 
-            # Assert
-            self.assertNotEqual(self.driver.current_url, self.HOME_PAGE_URL)
+        # Assert
+        self.assertFalse(self._login_page.password_input_field_is_displayed())
+
+    @JiraBugReporter.report_bug(
+        description="A correct email and a WRONG password are entered in the login process, but the website "
+                    "redirected the user to the home page, even though the password is wrong.",
+        priority="Highest",
+        labels=["Login", "Authentication", "Critical", "UI"]
+    )
+    def test_wrong_password_in_login(self):
+        """
+        This function tests if when inserting a correct email and wrong password,
+        then the login page prevents entering the website.
+        """
+        # Act
+        self._login_page.login_flow(self._config["asana_email"], self._config["wrong_password"])
+
+        # Assert
+        self.assertNotEqual(self._driver.current_url, self._config["base_url_app"])
