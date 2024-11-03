39424216d865f49d20b14b06e5525728927cd721
ShibelAl <shibelalshech300@gmail.com>
2024-11-03 16:51:11 +0200

    added function to check if the password input field is displayed + modified code functionality and organization

--- logic/ui/login_page.py
+++ logic/ui/login_page.py
@@ -1,52 +1,55 @@
-import logging
+from time import sleep
+from selenium.common import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as ec
+from infra.config_provider import ConfigProvider
 from infra.ui.base_page import BasePage
 
 
 class LoginPage(BasePage):
-    ASANA_GMAIL_INPUT = ('//input[@class = "TextInputBase SizedTextInput SizedTextInput--medium TextInput TextInput-'
-                         '-medium LoginEmailForm-emailInput"]')
-    ASANA_CONTINUE_BUTTON = '//div[text() = "Continue"]'
-    ASANA_PASSWORD_INPUT = '//input[@autocomplete = "current-password"]'
-    ASANA_LOGIN_BUTTON = '//div[text() = "Log in"]'
+    GMAIL_INPUT = ('//input[@class = "TextInputBase SizedTextInput SizedTextInput--medium TextInput '
+                   'TextInput--medium LoginEmailForm-emailInput"]')
+    CONTINUE_BUTTON = '//div[text() = "Continue"]'
+    PASSWORD_INPUT = '//input[@autocomplete = "current-password"]'
+    LOGIN_BUTTON = '//div[text() = "Log in"]'
 
     def __init__(self, driver):
         super().__init__(driver)
-        self._asana_gmail_input = self._driver.find_element(By.XPATH, self.ASANA_GMAIL_INPUT)
-        self._asana_continue_button = self._driver.find_element(By.XPATH, self.ASANA_CONTINUE_BUTTON)
-        self._asana_password_input = None
-        self._asana_login_button = None
+        self._config = ConfigProvider.load_config_json()
 
     def fill_email_input(self, email):
         """
         Fills the email input in the login page with the received parameter "email".
         :param email: string / email expression that the tester inserts to put in the email input
         """
-        self._asana_gmail_input.send_keys(email)
+        WebDriverWait(self._driver, self._config["wait_time"]).until(
+            ec.element_to_be_clickable((By.XPATH, self.GMAIL_INPUT))
+        ).send_keys(email)
 
     def click_on_continue_button(self):
         """
         Clicks on the continue button when inserting the email in the login.
         """
-        self._asana_continue_button.click()
+        WebDriverWait(self._driver, self._config["wait_time"]).until(
+            ec.element_to_be_clickable((By.XPATH, self.CONTINUE_BUTTON))
+        ).click()
 
     def fill_password_input(self, password):
         """
         Fills the password input with the parameter "password".
         :param password: a parameter that is represents the password in the login page.
         """
-        WebDriverWait(self._driver, 5).until(
-            EC.presence_of_element_located((By.XPATH, self.ASANA_PASSWORD_INPUT))
+        WebDriverWait(self._driver, self._config["wait_time"]).until(
+            ec.element_to_be_clickable((By.XPATH, self.PASSWORD_INPUT))
         ).send_keys(password)
 
     def click_on_login_button(self):
         """
         Clicks on the login button after filling the password.
         """
-        WebDriverWait(self._driver, 5).until(
-            EC.element_to_be_clickable((By.XPATH, self.ASANA_LOGIN_BUTTON))
+        WebDriverWait(self._driver, self._config["wait_time"]).until(
+            ec.element_to_be_clickable((By.XPATH, self.LOGIN_BUTTON))
         ).click()
 
     def login_flow(self, email, password):
@@ -55,8 +58,21 @@ class LoginPage(BasePage):
         :param email: a string that represents an email.
         :param password: a string that represents the password for the email.
         """
-        logging.info("Logging in to the website")
         self.fill_email_input(email)
         self.click_on_continue_button()
         self.fill_password_input(password)
         self.click_on_login_button()
+        sleep(self._config["ui_update_time"])
+
+    def password_input_field_is_displayed(self):
+        """
+        Checks whether the password input form is currently displayed on the page.
+        :return: True if the password input form is displayed, False otherwise.
+        """
+        try:
+            result = WebDriverWait(self._driver, self._config["wait_time"]).until(
+                ec.visibility_of_element_located((By.XPATH, self.PASSWORD_INPUT))
+            ).is_displayed()
+            return result
+        except TimeoutException:
+            return False
